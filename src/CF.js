5c8e5af533b8ee5bd842c812fb6ce6b6b36cae98
menglingyu659 <50974543+menglingyu659@users.noreply.github.com>
2020-10-25 16:48:51 +0800

    1.1.4

--- src/CF.js
+++ src/CF.js
@@ -3,6 +3,7 @@ import { Col, Form, Row } from "antd";
 import { createFormItemContent } from "./createFormItemContent";
 import { validatorKey } from "./utils";
 import Title from "./Title";
+import "./style/CF.css";
 
 const FormItem = Form.Item;
 
